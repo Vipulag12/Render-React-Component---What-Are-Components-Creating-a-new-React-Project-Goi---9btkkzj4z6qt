b69f7660d1de01990449d61380b63b13335815a2
Vipulag12 <97009659+Vipulag12@users.noreply.github.com>
2022-11-25 10:35:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-import{Task1} from "./react"
 
-const root =ReactDOM.render(<App />, document.getElementById("root"));
-root.render(
-    <Task1/>
-)
+ReactDOM.render(<App />, document.getElementById("root"));
+
